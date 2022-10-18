0782705fd1edc31466b6c953f594b3f5b5f54f61
Harrison Kangaho <harrison.kangaho28@gmail.com>
2022-10-18 16:07:01 -0700

    change edits

--- char_str_func.c
+++ char_str_func.c
@@ -1,28 +1,10 @@
 #include "main.h"
 
 /**
- * char_str_func - print a string
- * @list: the list of arguments the function _printf is receiving
- * Return: the number of characteres of the string
- */
-int func_string(va_list list)
-{
-	int i;
-	char *ptr;
-
-	ptr = (va_arg(list, char *));
-	if (ptr == 0)
-		ptr = "(null)";
-	for (i = 0; ptr[i] != 0; i++)
-		_putchar(ptr[i]);
-	return (i);
-}
-
-/**
- * func_char - print a char, or nothing if it is not
- * @list: the list of arguments the funcion id receiving
- * Return: 1 if it is a char, 0 otherwise
- */
+ ** func_char - print a char, or nothing if it is not
+ ** @list: the list of arguments the funcion id receiving
+ ** Return: 1 if it is a char, 0 otherwise
+ **/
 int func_char(va_list list)
 {
 	char c;
@@ -33,13 +15,27 @@ int func_char(va_list list)
 }
 
 /**
- * func_percent - print the sing percent
- * @list: the list of arguments the funcion id receiving
- * Return: 1 as just one of character
- */
+ ** func_char - print a char, or nothing if it is not
+ ** @list: the list of arguments the funcion id receiving
+ ** Return: 1 if it is a char, 0 otherwise
+ **/
+int func_char(va_list list)
+{
+	char s;
+
+	s = (va_arg(list, int));
+	_putchar(s);
+	return (1);
+}
+
+
+/**
+ ** func_percent - print the sing percent
+ ** @list: the list of arguments the funcion id receiving
+ ** Return: 1 as just one of character
+ **/
 int func_percent(__attribute__((unused)) va_list list)
 {
 	_putchar('%');
 	return (1);
 }
-
